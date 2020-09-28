3078b0be7ebfcb558ef4c88d447388a07c4a0331
George Mammen Kurien <59679846+george-mammen@users.noreply.github.com>
2020-09-28 10:11:46 +0530

    Update Iterative.java

--- Iterative.java
+++ Iterative.java
@@ -11,11 +11,12 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         int n=scan.nextInt();
-        int num;
         float sum=0;
-        for(int i=0;i<n;i++){
+        if(n<= 100 && n>=1){
+            for(int i=0;i<n;i++){
             sum+=scan.nextInt();
         }
+        }           
         System.out.println(sum/n);
     }
 }

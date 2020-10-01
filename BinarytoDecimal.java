376b4cb80417f29c24707e6c087a46539d7bac07
gouthamKKr <56692058+gouthamKKr@users.noreply.github.com>
2020-10-01 11:56:17 +0530

    Update BinarytoDecimal.java

--- BinarytoDecimal.java
+++ BinarytoDecimal.java
@@ -8,9 +8,9 @@ public class BinarytoDecimal {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         String s=scan.nextLine();
-        int len=s.length();
+        
         int num=0;
-        for(int i=len-1;i>=0;i--){
+        for(int i=s.length()-1;i>=0;i--){
             if(s.charAt(i)=='1')
                 num+=Math.pow(2,len-i-1);
         }

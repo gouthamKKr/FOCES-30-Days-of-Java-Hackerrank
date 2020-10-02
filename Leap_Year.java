7320461b1b882034f7840b43fbd6d292bca3fabe
macbmc <64430181+macbmc@users.noreply.github.com>
2020-10-02 23:59:26 +0530

    Update Leap_Year.java

--- Leap_Year.java
+++ Leap_Year.java
@@ -4,19 +4,19 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class Leap_yr {
 
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        int year=scan.nextInt();
-        if (year % 400 == 0) {
+        int yr=scan.nextInt();
+        if (yr % 400 == 0) {
             System.out.println("True");
         }
-        else if (year % 100 == 0) {
+        else if (yr % 100 == 0) {
             System.out.println("False");
         }
 
-        else if (year % 4 == 0) {
+        else if (yr % 4 == 0) {
             System.out.println("True");
         }
 
